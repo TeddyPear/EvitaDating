4d9ebe6e41b36d1222f6420206dd9d9c3a1e343b
TeddyPear <39911702+TeddyPear@users.noreply.github.com>
2018-06-04 15:27:39 +0200

    Create scripting.js

---
+++ scripting.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  var firstname1 = document.getElementById("output").innerHTML = document.getElementById("firstName1").value;
+  var lastname1 = document.getElementById("output").innerHTML = document.getElementById("lastName1").value;
+  var kinderen1 = document.getElementById("output").innerHTML = document.getElementById("Kinderen1").value;
+  var zelforg1 = document.getElementById("output").innerHTML = document.getElementById("zelfOrg1").value;
+  var anderorg1 = document.getElementById("output").innerHTML = document.getElementById("anderOrg1").value;
+}
