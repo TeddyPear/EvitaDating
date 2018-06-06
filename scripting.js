ea21c16180b2186e09f0f1206202bb10a860daf5
TeddyPear <39911702+TeddyPear@users.noreply.github.com>
2018-06-06 07:47:22 +0200

    Update scripting.js

--- scripting.js
+++ scripting.js
@@ -1,17 +1,25 @@
 function myFunction() {
-  function persoon(naam, kinderen, leeftijd){
+  function persoon(naam, kinderen, leeftijd, zelforg, anderorg){
     this.naam = naam;
     this.kinderen = kinderen;
     this.leeftijd = leeftijd;
+    this.zelforg = zelforg;
+    this.anderorg = anderorg;
 }
 var kandidaten = [];
 
-var zelf = new persoon("Emiel", "Ja",15);
+var zelf = new persoon(
+  document.getElementById("firstName1").value,
+  document.getElementById("Kinderen1").value,
+  document.getElementById("leeftijd").value,
+  document.getElementById("zelfOrg1").value,
+  document.getElementById("anderOrg1").value
+  );
   
-kandidaten.push(new persoon("henk","Ja",20));
-kandidaten.push(new persoon("Jan","Nee", 19));
-kandidaten.push(new persoon("Eva","Ja",22));
-kandidaten.push(new persoon("Julie","Nee",23));
+kandidaten.push(new persoon("henk","Ja",20,"Ja","Maakt niet uit"));
+kandidaten.push(new persoon("Jan","Nee", 19,"Ja","Liever wel"));
+kandidaten.push(new persoon("Eva","Ja",22,"Nee","Liever wel"));
+kandidaten.push(new persoon("Julie","Nee",23,"Nee","Maakt niet uit"));
   
 var scores = [];
   
@@ -20,6 +28,15 @@ for(i = 0; i < kandidaten.length;i++){
   if(zelf.kinderen == kandidaten[i].kinderen){
     punten += 10;
   }
+  punten += Math.max(5 - (Math.abs(zelf.leeftijd - kandidaten[i].leeftijd)/2),0);
+  if((zelf.anderorg == "Maakt niet uit" )||(kandidaten[i].zelforg == "Ja")){
+     punten += 4;
+  }
+  if((kandidaten[i].anderorg == "Maakt niet uit" )||(zelf.zelforg == "Ja")){
+     punten += 4;
+  }
+    
+    
   scores.push(punten); 
 }  
 var max = 0;
@@ -30,6 +47,7 @@ for(i = 0; i < scores.length ;i++){
    max = scores[i];
    plek = i;
   }
+  
 }  
   document.getElementById("output").innerHTML = kandidaten[plek].naam;
 }
